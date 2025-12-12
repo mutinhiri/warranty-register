c055b41fef1033ba3665fb4bd07efbf50325ecf2
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-12-12 15:26:32 +0200

    Add server

--- app/main.py
+++ app/main.py
@@ -6,14 +6,18 @@ from . import models, database, routes
 app = FastAPI(title="Warranty Register API")
 
 
+origins = [
+    "https://asset-management-git-dev-mutinhiris-projects.vercel.app",
+    "https://server4.eport.ws",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or ["https://server4.eport.ws"] in production
+    allow_origins=origins,  # cannot use "*" with credentials
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # Create tables
 models.Base.metadata.create_all(bind=database.engine)
 
