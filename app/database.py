24c5761ec3935f952c09b32c4ac5d59b58c00ed4
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-12-12 17:05:06 +0200

    Change db url

--- app/database.py
+++ app/database.py
@@ -9,7 +9,8 @@ DATABASE_URL = os.getenv(
     # "postgresql://postgres:subaru@localhost:5432/warranty_db"
     # "postgresql://eportuser:eportpassword@localhost:5432/warranty_db"
    # instead of localhost
-"postgresql://eportuser:eportpassword@warranty-register-db-1:5432/postgres"
+   "postgresql://eportuser:eportpassword@warranty-register-db-1:5432/warranty"
+
 
 
 
