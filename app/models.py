a2a5f3ce4f80efa00453b5b999b825f56b878d42
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-12-13 13:02:16 +0200

    asset_name remove

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Warranty(Base):
     __tablename__ = "warranties"
     id = Column(Integer, primary_key=True, index=True)
     asset_id = Column(String, index=True)
-    asset_name = Column(String)
+    # asset_name = Column(String)
     asset_serial = Column(String)
     registered_by = Column(String)
     registered_at = Column(DateTime(timezone=True), server_default=func.now())
--- app/schemas.py
+++ app/schemas.py
@@ -5,12 +5,12 @@ class WarrantyCreate(BaseModel):
     asset_id: str
     asset_serial: str
     registered_by: str
-    asset_name: str
+    # asset_name: str
 
 class WarrantyOut(BaseModel):
     id: int
     asset_id: str
-    asset_name: str
+    # asset_name: str
     asset_serial: str
     registered_by: str
     registered_at: datetime
