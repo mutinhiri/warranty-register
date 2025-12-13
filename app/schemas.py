5e018ba2087d76db82bdfab403c62f7a0fa9c3db
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2025-12-13 12:21:05 +0200

    Add schema queries

--- app/schemas.py
+++ app/schemas.py
@@ -5,10 +5,12 @@ class WarrantyCreate(BaseModel):
     asset_id: str
     asset_serial: str
     registered_by: str
+    asset_name: str
 
 class WarrantyOut(BaseModel):
     id: int
     asset_id: str
+    asset_name: str
     asset_serial: str
     registered_by: str
     registered_at: datetime
